dcd76ed39e156a1f1d66d023a7cbe159cd46920b
starbudz <victorngigi@gmail.com>
2018-08-20 10:50:06 +0300

    added background

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+// front end begin
+$(document).ready(function() {
+        $(".begin").click(function() {
+          $("#questions .item").slice(0, 4).fadeIn("slow");
+          $(".intro").fadeOut("slow");
+          $(".next").show();
+
+          $(".next").click(function() {
+
+            var items = $('#questions .item:visible').fadeOut("slow").last();
+
+            var nextItems = items.nextAll().slice(0, 4);
+
+            if (nextItems.length === 1) {
+              nextItems = $("#questions .item").slice(-4);
+            }
+
+            if (nextItems.length === 0) {
+              nextItems = $("#questions .item").slice(0, 4);
+            } else $(".submit").show();
+            $(".next").hide();
+
+            nextItems.fadeIn("slow");
+          });
+        });
+      });
+// front end fin
+// back end begin
 function calculateScore(){
 
  var correctAnswers = ['a','d','b','b','c','c','d','b'];
@@ -5,82 +33,100 @@ function calculateScore(){
  var point = 10;
  var answer = [];
 
- for(var n = 0; n < correctAnswers.length; n++){
-     if(correctAnswers[n] == $("input[name='question"+(n+1)+"]:checked").val()){
-       score = score + point;
-       console.log('question'+(n+1)+': correct');
-     }else{
-       score = score + 0;
-       console.log('question'+(n+1)+': wrong');
+   for(var n = 0; n < correctAnswers.length; n++){
+       if(correctAnswers[n] == jQuery("input[name='question"+(n+1)+"']:checked").val()){
+         score = score + point;
+         console.log('question'+(n+1)+': correct');
+       }else{
+         score = score + 0;
+         console.log('question'+(n+1)+': wrong');
+       }
      }
+
+     console.log('your score is: '+score);
+
+     //lets display the score in the div element
+     jQuery('.result').html('Your Score: '+score);
+
+     //show element since we've hid it in csscalculateScore()
+     jQuery('.result').show();
+
+     scrollUp();
    }
 
-   console.log('your score is: '+score);
 
-   //lets display the score in the div element
-   $('.result').html('Your Score: '+score);
+ function scrollUp(){
+     $("html, body").animate({
+         scrollTop: $(document).height()
+     }, 100);
 
-   //show element since we've hid it in css
-   $('.result').show();
-
-   scrollUp();
+     //scroll back up
+     $("html, body").animate({
+         scrollTop: 0
+     }, 100);
  }
 
+ function validate(){
+   calculateScore();
 
-function scrollUp(){
-   $("html, body").animate({
-       scrollTop: $(document).height()
-   }, 100);
+     if(jQuery("input[name='question1']").is(':checked') == false){
+         //show element since we've hid it in css
+         jQuery('.result').show();
+         jQuery('.result').text('Please select the first answer');
+         scrollUp();
+         return false;
+     }else if(jQuery("input[name='question2']").is(':checked') == false){
+         //show element since we've hid it in css
+         jQuery('.result').show();
+         jQuery('.result').text('Please select the second answer');
+         scrollUp();
+         return false;
+     }else if(jQuery("input[name='question3']").is(':checked') == false){
+         //show element since we've hid it in css
+         jQuery('.result').show();
+         jQuery('.result').text('Please select the third answer');
+         scrollUp();
+         return false;
+     }
+     else if(jQuery("input[name='question4']").is(':checked') == false){
+           //show element since we've hid it in css
+           jQuery('.result').show();
+           jQuery('.result').text('Please select the final answer');
+           scrollUp();
+           return false;
+     }else if(jQuery("input[name='question5']").is(':checked') == false){
+           //show element since we've hid it in css
+           jQuery('.result').show();
+           jQuery('.result').text('Please select the final answer');
+           scrollUp();
+           return false;
+     }else if(jQuery("input[name='question6']").is(':checked') == false){
+           //show element since we've hid it in css
+           jQuery('.result').show();
+           jQuery('.result').text('Please select the final answer');
+           scrollUp();
+           return false;
+     }else if(jQuery("input[name='question7']").is(':checked') == false){
+           //show element since we've hid it in css
+           jQuery('.result').show();
+           jQuery('.result').text('Please select the final answer');
+           scrollUp();
+           return false;
+     }else if(jQuery("input[name='question8']").is(':checked') == false){
+           //show element since we've hid it in css
+           jQuery('.result').show();
+           jQuery('.result').text('Please select the final answer');
+           scrollUp();
+           return false;
+     }else{
+         calculateScore();
+     }
 
-   //scroll back up
-   $("html, body").animate({
-       scrollTop: 0
-   }, 100);
-}
 
-function validate(){
+ }
 
-   if($("input[name='question"+(n+1)+"]:checked") == false){
-       //show element since we've hid it in css
-       $('.result').show();
-       $('.result').text('Please answer all questions');
-       scrollUp();
-       return false;
-   }else{
-       calculateScore();
-   }
+ function refresh(){
 
-
-}
-
-function refresh(){
-
-  $('.result').hide();
-  scrollUp();
-}
-    $(document).ready(function() {
-            $(".begin").click(function() {
-              $("#questions .item").slice(0, 4).fadeIn("slow");
-              $(".intro").fadeOut("slow");
-              $(".next").show();
-
-              $(".next").click(function() {
-
-                var items = $('#questions .item:visible').fadeOut("slow").last();
-
-                var nextItems = items.nextAll().slice(0, 4);
-
-                if (nextItems.length === 1) {
-                  nextItems = $("#questions .item").slice(-4);
-                }
-
-                if (nextItems.length === 0) {
-                  nextItems = $("#questions .item").slice(0, 4);
-                } else $(".submit").show();
-                $(".next").hide();
-
-                nextItems.fadeIn("slow");
-              });
-            });
-            $(".submit").click(calculateScore());
-          });
+    jQuery('.result').hide();
+    scrollUp();
+ }
